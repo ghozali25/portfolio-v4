1ec64feb1b9e08b7c04654eeb4d5555a6c8a28f7
Ali <ghozali.ahmd25@gmail.com>
2025-11-09 19:59:22 +0700

    fix position

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -161,7 +161,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-[#030014] overflow-hidden" id="Home">
       <div className={`relative z-10 transition-all duration-1000 ${isLoaded ? "opacity-100" : "opacity-0"}`}>
-        <div className="container mx-auto px-[5%] sm:px-6 lg:px-[0%] min-h-screen">
+        <div className="container mx-auto px-[5%] sm:px-6 lg:px-[10%] min-h-screen">
           <div className="flex flex-col lg:flex-row items-center justify-center h-screen md:justify-between gap-0 sm:gap-12 lg:gap-20">
             {/* Left Column */}
             <div className="w-full lg:w-1/2 space-y-6 sm:space-y-8 text-left lg:text-left order-1 lg:order-1 lg:mt-0"
