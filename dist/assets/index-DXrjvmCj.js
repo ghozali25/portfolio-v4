911922ec64a4c478e3046ff35c1f04a7b314882c
ghozali25 <ghozali.ahmd25@gmail.com>
2026-05-05 16:04:15 +0700

    add

---
+++ dist/assets/index-DXrjvmCj.js
@@ -0,0 +1,1 @@
+import{f as r,g as t}from"./index-Rl9XndT3.js";var a=r();const e=t(a);export{e as R,a as r};
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, memo, useMemo, useState } from "react"
-import GitHubCalendar from 'react-github-calendar';
+import * as GitHubCalendarNamespace from 'react-github-calendar';
 import { supabase } from "../lib/supabaseClient"
 import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck, Github } from "lucide-react"
 import AOS from 'aos'
@@ -315,7 +315,7 @@ const AboutPage = () => {
             </div>
 
             <div className="flex justify-center w-full overflow-hidden">
-              <GitHubCalendar 
+              <GitHubCalendarNamespace.default 
                 username="ghozali25"
                 blockSize={12}
                 blockMargin={4}
